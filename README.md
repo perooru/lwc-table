35e7db608933df0003a71f777eacd78712235d9f
perooru <37695479+undefined@users.noreply.github.com>
2022-02-02 10:43:58 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lwc-table
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+  title = "Welcome to Lightning Web Components!";
+
+  
+}
